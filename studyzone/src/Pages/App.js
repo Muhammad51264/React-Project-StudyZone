51da77d151722ba1e9dab91536aeae4cf9ef9b4d
Muhammad51264 <muh.freijat@gmail.com>
2023-06-19 10:15:55 +0300

    ADDING-ROUTES-TO-PAGES

--- studyzone/src/Pages/App.js
+++ studyzone/src/Pages/App.js
@@ -1,9 +1,32 @@
 import '../Styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import Courses from "../Pages/Courses";
+import CourseDetails from "../Pages/CourseDetails";
+import Login from "../Pages/Login";
+import Register from '../Pages/Register';
+import Card from '../Components/Card'; 
 function App() {
   return (
-    <div className="App">
-      Test
-    </div>
+    <>
+    <Card/>
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" Component={}></Route> */}
+      <Route path='/login' Component={Login}></Route>
+      <Route path='/register' Component={Register}></Route>
+      <Route path='/courses' Component={Courses}></Route>
+      <Route path='/courses/:courseId' Component={CourseDetails}></Route>
+    
+    </Routes>
+    </BrowserRouter>
+    
+    
+    
+    
+    </>
+
   );
 }
 
