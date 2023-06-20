cb8bef3aefda56b2caf6711c008086ff7e88f86f
mohammad <moh.hsaadeh@gmail.com>
2023-06-20 15:11:53 +0300

    add button to nav Bar

--- studyzone/src/Components/Nav.js
+++ studyzone/src/Components/Nav.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { FaSistrix } from "react-icons/fa";
-
+import NavButton from "./NavButton"
 const Nav = () => {
   return (
     <nav className="navbar_">
-      <div className="navbar__logo">Logo</div>
-
-
+      <div className="navbar__logo">
+        <img src='../Images/logo-no-background.png'></img>
+      </div>
       <div class="navbar__search">
   <span><FaSistrix /></span>
   <input type="text" placeholder="Search for anythings" />
 </div>
-
       <div className="navbar__text">Right-aligned text</div>
+
+    <NavButton/>
+
     </nav>
   );
 }
---
+++ studyzone/src/Components/NavButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function NavButton() {
+  return (
+    <div className="navbar__button">
+       <button className='btnLogin'>
+        Login
+       </button>
+
+       <button className='btnSignUp'>
+       Sign up
+       </button>
+
+
+    </div>
+  )
+}
+
+export default NavButton
