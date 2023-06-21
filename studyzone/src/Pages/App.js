f4a6a93c6b30ee122947b6c99d12d9c3ff816be8
Qais Abburisheh <qais.aburisheh@gmail.com>
2023-06-21 23:40:00 +0300

    filter btn by category done

--- studyzone/src/Pages/App.js
+++ studyzone/src/Pages/App.js
@@ -1,29 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import '../Styles/App.css';
-import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "../Styles/App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Courses from "../Pages/Courses";
 import CourseDetails from "../Pages/CourseDetails";
 import Login from "../Pages/Login";
-import Register from '../Pages/Register';
-import LandingPage from './LandingPage';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Nav from '../Components/Nav';
+import Register from "../Pages/Register";
+import LandingPage from "./LandingPage";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Nav from "../Components/Nav";
 function App() {
   return (
     <>
-    <Nav/>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" Component={LandingPage}></Route>
-      <Route path='/login' Component={Login}></Route>
-      <Route path='/register' Component={Register}></Route>
-      <Route path='/courses' Component={Courses}></Route>
-      <Route path='/courses/courseId' Component={CourseDetails}></Route>
-    </Routes>
-    </BrowserRouter>
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={LandingPage}></Route>
+          <Route path="/login" Component={Login}></Route>
+          <Route path="/register" Component={Register}></Route>
+          <Route path="/courses" Component={Courses}></Route>
+          <Route path="/courses/courseId" Component={CourseDetails}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-    
   );
 }
 
--- studyzone/src/Pages/Courses.js
+++ studyzone/src/Pages/Courses.js
@@ -1,49 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Components/Card";
 import { Link } from "react-router-dom";
 import CardSlider from '../Components/Card_Slider';
 import CoursesInfo from "./CoursesInfo";
 
-import { useState } from "react";
-
 const Courses = () => {
-  /*
-  const courses = [
-    {
-      title: "Course 3",
-      desc: "Description of Course 2",
-      price: "$59",
-      categoryLogo: "Images/programming.svg",
-      category: "Programming",
-      hours: "45 Hours",
-    },
-    {
-      title: "Course 4",
-      desc: "Description of Course 2",
-      price: "$59",
-      categoryLogo: "Images/programming.svg",
-      category: "Programming",
-      hours: "45 Hours",
-    },
+ const [filters, setFilters] = useState("");
 
-  ];*/
-
-  // recomended section
-  // const recommendedCourses = courses.slice(0, 4);
-
-  const [filters, setFilters] = useState("");
-// filter function to filter the courses according to hours
-  /*const filter1 = () => {
-    setFilters("45 Hours");
+  // Filter function to filter the courses according to category
+  const filterCourses = (category) => {
+    setFilters(category);
   };
 
-  const filteredCourses = courses.filter((course) => {
-    return course.hours === filters;
+  const filteredCourses = CoursesInfo.filter((course) => {
+    return course.category === filters;
   });
-
-  const filter2 = () => {
-    setFilters("30 Hours");
-  };*/
 
   return (
     <>
@@ -53,7 +24,7 @@ const Courses = () => {
       <div className="container my-5">
         <div className="courses--heading">
           <h3 className="courses--left">Our Courses</h3>
-          {/* <div className="dropdown courses--filterbtn">
+          <div className="dropdown courses--filterbtn">
             <button
               className="btn"
               type="button"
@@ -64,63 +35,55 @@ const Courses = () => {
             </button>
             <ul className="dropdown-menu dropdown-menu-end">
               <li>
-                <Link onClick={filter1} className="dropdown-item" href="#">
-                  45 hours
+              <Link
+              onClick={() => filterCourses("programming")}
+              className="dropdown-item"
+              href="#"
+            >
+              Programming
+            </Link>
+              </li>
+              <li>
+                <Link onClick={() => filterCourses("Marketing")}  className="dropdown-item" href="#">
+                  Marketing
                 </Link>
               </li>
               <li>
-                <Link onClick={filter2} className="dropdown-item" href="#">
-                  30 hours
-                </Link>
-              </li>
-              <li>
-                <Link className="dropdown-item" href="#">
-                  Something else here
+                <Link onClick={() => filterCourses("Design")} className="dropdown-item" href="#">
+                  Design
                 </Link>
               </li>
             </ul>
-          </div> */}
+          </div>
         </div>
-        <div className="cards--container">
-        {CoursesInfo[0].courses.map((course) => (
-  <Card
-    key={course.ID}
-    width="350px"
-    imgLink={course.image}
-    title={course["Course Name"]}
-    desc={course.description}
-    price={course.Price}
-    categoryLogo={course.categoryLogo}
-    category={course.Category}
-    hours={course.hours}
-  />
-))}
-          {/* {filters
-            ? filteredCourses.map((course, index) => (
-                <Card
-                  key={index}
-                  width="350px"
-                  title={course.title}
-                  desc={course.desc}
-                  price={course.price}
-                  categoryLogo={course.categoryLogo}
-                  category={course.category}
-                  hours={course.hours}
-                />
-              ))
-            : courses.map((course, index) => (
-                <Card
-                  key={index}
-                  width="350px"
-                  title={course.title}
-                  desc={course.desc}
-                  price={course.price}
-                  categoryLogo={course.categoryLogo}
-                  category={course.category}
-                  hours={course.hours}
-                />
-              ))} */}
-        </div>
+        <div className="cards--container "> {filters
+          ? filteredCourses.map((course) => (
+            <Card
+            key={course.id}
+            width="350px"
+            imgLink={course.image}
+            title={course.courseName}
+            desc={course.description}
+            price={course.price}
+            categoryLogo={course.categoryLogo}
+            category={course.category}
+            hours={course.hours}
+          />
+            ))
+          : CoursesInfo.map((course) => (
+            <Card
+            key={course.id}
+            width="350px"
+            imgLink={course.image}
+            title={course.courseName}
+            desc={course.description}
+            price={course.price}
+            categoryLogo={course.categoryLogo}
+            category={course.category}
+            hours={course.hours}
+          />
+            ))}
+      </div>
       </div>
       <div className="courses--recommended--background">
         <div className="container courses--recommended--heading">
@@ -140,5 +103,4 @@ const Courses = () => {
     </>
   );
 };
-
 export default Courses;
