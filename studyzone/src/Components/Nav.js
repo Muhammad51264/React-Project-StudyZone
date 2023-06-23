a545c1732793143ab42b1e728b67a6dc2cdab13d
Mohammad Saadeh <moh.hsaadeh@gmail.com>
2023-06-23 21:12:16 +0300

    Create Search input as a new component and make it when input key enter search by course name

--- studyzone/src/Components/Nav.js
+++ studyzone/src/Components/Nav.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { FaSistrix } from "react-icons/fa";
 import NavButton from "./NavButton"
 import NavIcons from './navIcons';
+import NavSearch from './navSearch';
 const Nav = () => {
   return (
     <nav className="navbar_">
       <div className="navbar__logo">
         <img src='../Images/logo-no-background.png'></img>
       </div>
-      <div className="navbar__search">
-  <span><FaSistrix /></span>
-  <input type="text" placeholder="Search for anythings" />
-</div>
+      <NavSearch/>
  
 
     {/* <NavButton/> */}
---
+++ studyzone/src/Components/navSearch.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { FaSistrix } from 'react-icons/fa';
+import CoursesInfo from '../Pages/CoursesInfo';
+
+const NavSearch = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleInputChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+
+    // Filter the courses based on the search query
+    const filteredResults = CoursesInfo.filter((course) =>
+      course.courseName.toLowerCase().includes(query.toLowerCase())
+    );
+
+    setSearchResults(filteredResults);
+  };
+
+  return (
+    <>
+      <div className="navbar__search">
+        <span>
+          <FaSistrix />
+        </span>
+        <input
+          type="text"
+          placeholder="Search courses..."
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        {searchQuery && (
+          <ul className="search_dropdown">
+            {searchResults.length > 0 ? (
+              searchResults.map((course) => (
+                <li key={course.id}>
+                  <img src={course.image} alt={course.courseName} />
+                  {course.courseName}
+                </li>
+              ))
+            ) : (
+              <li>No courses found.</li>
+            )}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NavSearch;
